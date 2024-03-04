7549b026400811ad7f82297856524436b1d38642
Edgar Otieno <eddyade98@gmail.com>
2024-03-04 16:48:02 +0300

    Completed Assignment

--- server/seed.py
+++ server/seed.py
@@ -4,8 +4,8 @@ from random import randint, choice as rc
 
 from faker import Faker
 
-from app import app
-from models import db, Game, Review, User
+from server.app import app
+from models import db, Game, Review, User 
 
 genres = [
     "Platformer",
